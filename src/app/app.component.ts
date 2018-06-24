690bd6a2b6d11c87d44ec447016209f869fbedd4
Wendell <wendzhue@gmail.com>
2018-06-24 10:36:43 +0800

    Improvement: Modal and shortcuts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from './services/local-storage/local-storage.service';
-import { floorToDate } from '../utils/time';
-import { SummaryService } from './services/summary/summary.service';
-import {
-  START_USING_DATE,
-  INIT_FLAG
-} from './services/local-storage/local-storage.namespace';
 
-/**
- * We are going to write app lifecycle methods here.
- * Be aware that app destorying lifecycle hooks are not reliable.
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private store: LocalStorageService,
-    private summaryService: SummaryService
-  ) {}
+  constructor() { }
 
   ngOnInit() {
-    // App initializaiton.
-    this.summaryService.doSummary();
+    window.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
   }
 }
--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { combineLatest } from 'rxjs/operators';
@@ -17,25 +17,28 @@ import { floorToDate } from '../../utils/time';
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.less']
+  styleUrls: [ './core.component.less' ]
 })
 export class CoreComponent implements OnInit, OnDestroy {
   private isCollapsed = false;
 
   private todos$: Subscription;
   private lists$: Subscription;
+  private sort$: Subscription;
+  private filter$: Subscription;
 
   private innerTodos: Todo[] = [];
   private innerLists: List[] = [];
-
   private addTodoModalVisible = false;
   private addListModalVisible = false;
   private renameListModalVisible = false;
-
-  private tempUUID: string;
-
+  private templateListUUID: string;
   private avatar = this.store.get(AVATAR_CODE);
   private username = this.store.get(USERNAME);
+
+  @ViewChild('todoInput') private todoInput: ElementRef;
+  @ViewChild('listRenameInput') private listRenameInput: ElementRef;
+  @ViewChild('listInput') private listInput: ElementRef;
 
   constructor(
     private todoService: TodoService,
@@ -43,40 +46,61 @@ export class CoreComponent implements OnInit, OnDestroy {
     private router: Router,
     private modal: NzModalService,
     private store: LocalStorageService
-  ) {}
+  ) { }
+
+  private keyboard$: any;
 
   ngOnInit() {
-    if (!this.store.get(INIT_FLAG)) {
-      this.router.navigateByUrl('/setup');
-      return;
-    }
+    if (!this.store.get(INIT_FLAG)) { this.router.navigateByUrl('/setup'); return; }
     this.makeSubcriptions();
+    // We have to bind this here. Otherwise, you know this would be bind to undefined.
+    this.keyboard$ = this.keyboardHandler.bind(this);
+    window.addEventListener('keydown', this.keyboard$);
   }
 
   ngOnDestroy() {
-    if (this.lists$) {
-      this.lists$.unsubscribe();
-    }
-    if (this.todos$) {
-      this.todos$.unsubscribe();
-    }
+    if (this.lists$) { this.lists$.unsubscribe(); }
+    if (this.todos$) { this.todos$.unsubscribe(); }
+    window.removeEventListener('keydown', this.keyboard$);
   }
 
   private makeSubcriptions(): void {
     const listSource = this.listService.getSubject();
-    const currentUUIDSource = this.listService.getCurrentSubject();
-    const todoSource = this.todoService.getSubject();
-    const combined = currentUUIDSource.pipe(combineLatest(todoSource));
 
     this.lists$ = listSource.subscribe((lists: List[]) => {
       this.innerLists = [].concat(lists);
     });
+
+    const currentUUIDSource = this.listService.getCurrentSubject();
+    const todoSource = this.todoService.getSubject();
+    const combined = currentUUIDSource.pipe(combineLatest(todoSource));
+
     this.todos$ = combined.subscribe(sources => {
-      this.processTodos(sources[0], sources[1]);
+      this.processTodos(sources[ 0 ], sources[ 1 ]);
     });
 
     this.listService.getAll();
     this.todoService.getAll();
+  }
+
+  private keyboardHandler(e: KeyboardEvent): void {
+    // command+U to create a new list
+    if (e.keyCode === 85 && (e.metaKey || e.ctrlKey)) {
+      this.showAddListModal();
+      return;
+    }
+
+    // command+I to create a new todo
+    if (e.keyCode === 73 && (e.metaKey || e.ctrlKey)) {
+      this.showAddTodoModal();
+      return;
+    }
+
+    if (e.keyCode === 27) {
+      this.closeAddListModal();
+      this.closeAddTodoModal();
+      this.closeRenameListModal();
+    }
   }
 
   private processTodos(uuid: string, todos: Todo[]): void {
@@ -86,15 +110,9 @@ export class CoreComponent implements OnInit, OnDestroy {
           uuid === 'today' &&
           todo.planAt &&
           floorToDate(todo.planAt) === floorToDate(new Date())
-        ) {
-          return true;
-        }
-        if (uuid === 'todo' && (!todo.listUUID || todo.listUUID === 'todo')) {
-          return true;
-        }
-        if (uuid === todo.listUUID) {
-          return true;
-        }
+        ) { return true; }
+        if (uuid === 'todo' && (!todo.listUUID || todo.listUUID === 'todo')) { return true; }
+        if (uuid === todo.listUUID) { return true; }
         return false;
       })
       .map(todo => {
@@ -109,19 +127,26 @@ export class CoreComponent implements OnInit, OnDestroy {
   /* events on modals */
   private showAddTodoModal(): void {
     this.addTodoModalVisible = true;
+    setTimeout(() => { this.todoInput.nativeElement.focus(); });
   }
   private closeAddTodoModal(): void {
     this.addTodoModalVisible = false;
   }
   private showAddListModal(): void {
     this.addListModalVisible = true;
+    setTimeout(() => { this.listInput.nativeElement.focus(); });
   }
   private closeAddListModal(): void {
     this.addListModalVisible = false;
   }
   private showRenameListModal(uuid: string): void {
     this.renameListModalVisible = true;
-    this.tempUUID = uuid;
+    this.templateListUUID = uuid;
+    setTimeout(() => {
+      const title = this.innerLists.find(l => l._id === uuid).title;
+      this.listRenameInput.nativeElement.value = title;
+      this.listRenameInput.nativeElement.focus();
+    });
   }
   private closeRenameListModal(): void {
     this.renameListModalVisible = false;
@@ -129,14 +154,12 @@ export class CoreComponent implements OnInit, OnDestroy {
 
   /* events on lists */
   private handleAddListOk(title: string): void {
-    if (title) {
-      this.listService.add(title);
-    }
+    if (title) { this.listService.add(title); }
     this.closeAddListModal();
   }
   private confirmDeleteList(uuid: string): void {
     const i = this.innerLists.findIndex(l => l._id === uuid);
-    const list = this.innerLists[i];
+    const list = this.innerLists[ i ];
 
     this.modal.confirm({
       nzTitle: '确认删除列表',
@@ -150,11 +173,11 @@ export class CoreComponent implements OnInit, OnDestroy {
     });
   }
   private renameList(title: string): void {
-    const list = this.innerLists.find(l => l._id === this.tempUUID);
+    const list = this.innerLists.find(l => l._id === this.templateListUUID);
     list.title = title;
     this.listService.update(list);
     this.closeRenameListModal();
-    this.tempUUID = '';
+    this.templateListUUID = '';
   }
   private clickList(uuid: string): void {
     this.listService.setCurrentUUID(uuid);
